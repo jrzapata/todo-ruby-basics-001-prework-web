b922484e67d61bdfaa0eeb1bbe1ea6b4afde9ef3
jrzapata <jordan.r.zapata@tcu.edu>
2015-10-01 01:24:37 -0500

    that was hard

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
+division(12, 2)
+
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+
+def argue(phrase = "I'm right and you are wrong!")
+  return phrase 
 end
 
-def greeting
+def greeting(phrase1, phrase2)
+end
+
+
+def return_a_value(other = "Nice")
+  return other
 
 end
 
-def return_a_value
-
+def last_evaluated_value(last = "expert")
+  return last
 end
 
-def last_evaluated_value
-
+def pizza_party(arg1 = "cheese")
+  return arg1
 end
-
-def pizza_party
-
-end
+pizza_party("pepperoni")
